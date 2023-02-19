d9bc81134f09f8c00afd09deaf514e0d045a1ee0
Pavel Rozhkov <pp12233@yandex.ru>
2023-02-19 23:26:28 +0300

    9pr 1fix

--- src/scripts/components/Api.js
+++ src/scripts/components/Api.js
@@ -4,91 +4,69 @@ export default class Api {
     this._headers = data.headers;
   }
 
-  _checkResponse(res) {
-      if (res.ok) {return res.json()}
-      return Promise.reject(`Ошибка ${res.status}`)
+  _request(url, options) {
+    return fetch(url, options).then(this._checkResponse);
   }
 
+  _checkResponse(res) {
+    if (res.ok) {
+      return res.json();
+    }
+    return Promise.reject(`Ошибка ${res.status}`);
+  }
 
+  //1. Загрузка информации о пользователе с сервера
+  getUserInfo = () =>
+    this._request(this._baseUrl + "/users/me", { headers: this._headers });
 
-//1. Загрузка информации о пользователе с сервера
-getUserInfo() {
-  return fetch(this._baseUrl + '/users/me', {
-    headers: this._headers
-  })
-    .then(this._checkResponse)
-}
+  //2. Загрузка карточек с сервера
+  getCards = () =>
+    this._request(this._baseUrl + "/cards", { headers: this._headers });
 
-//2. Загрузка карточек с сервера
-getCards() {
-  return fetch(this._baseUrl + '/cards', {
-    headers: this._headers
-  })
-    .then(this._checkResponse)
-}
+  //3. Редактирование профиля
+  editingUser = (data) =>
+    this._request(this._baseUrl + "/users/me", {
+      method: "PATCH",
+      headers: this._headers,
+      body: JSON.stringify({ name: data.name, about: data.about }),
+    });
 
-//3. Редактирование профиля
-editingUser(data) {
-  return fetch(this._baseUrl + '/users/me', {
-    method: 'PATCH',
-    headers: this._headers,
-    body: JSON.stringify({
-      name: data.name,
-      about: data.about
-    })
-  })
-  .then(this._checkResponse)
-}
+  //4. Добавление новой карточки
+  addingNewCard = (data) =>
+    this._request(this._baseUrl + "/cards", {
+      method: "POST",
+      headers: this._headers,
+      body: JSON.stringify({ name: data.city, link: data.link }),
+    });
 
-//4. Добавление новой карточки
-addingNewCard(data) {
-  return fetch(this._baseUrl + '/cards', {
-    method: 'POST',
-    headers: this._headers,
-    body: JSON.stringify({
-      name: data.city,
-      link: data.link
-    })
-  })
-  .then(this._checkResponse)
-}
+  //7. Удаление карточки
+  deletingCard = (_id) =>
+    this._request(this._baseUrl + "/cards/" + _id, {
+      method: "DELETE",
+      headers: this._headers,
+    });
 
-//7. Удаление карточки
-deletingCard(_id) {
-  return fetch(this._baseUrl + '/cards/' + _id, {
-    method: 'DELETE',
-    headers: this._headers
-  })
-    .then(this._checkResponse)
-}
+  //8. Постановка лайка
+  settingLike = (_id) =>
+    this._request(this._baseUrl + "/cards/" + _id + "/likes", {
+      method: "PUT",
+      headers: this._headers,
+    });
 
-//8. Постановка лайка
-settingLike(_id) {
-  return fetch(this._baseUrl + '/cards/' + _id + '/likes', {
-    method: 'PUT',
-    headers: this._headers
-  })
-    .then(this._checkResponse)
-}
+  //8. Снятие лайка
+  removingLike = (_id) =>
+    this._request(this._baseUrl + "/cards/" + _id + "/likes", {
+      method: "DELETE",
+      headers: this._headers,
+    });
 
-//8. Снятие лайка
-removingLike(_id) {
-  return fetch(this._baseUrl + '/cards/' + _id + '/likes', {
-    method: 'DELETE',
-    headers: this._headers
-  })
-    .then(this._checkResponse)
-}
-
-//9. Обновление аватара пользователя
-updateAvatar(data) {
-  return fetch(this._baseUrl + '/users/me/avatar', {
-    method: 'PATCH',
-    headers: this._headers,
-    body: JSON.stringify({
-      avatar: data.link
-    })
-  })
-  .then(this._checkResponse)
-}
+  //9. Обновление аватара пользователя
+  updateAvatar = (data) =>
+    this._request(this._baseUrl + "/users/me/avatar", {
+      method: "PATCH",
+      headers: this._headers,
+      body: JSON.stringify({
+        avatar: data.link,
+      }),
+    });
 }
